ecc635a079f5ba5df7b44a868c5c58329d0579c5
Armontex <windle1337@gmail.com>
2026-02-07 20:24:59 +0500

    docs(seller): add docstrings to BaseMarketAnalyzer, StandardMarketAnalyzer, SellItemUseCase class

--- services/seller/app/usecases/sell_item.py
+++ services/seller/app/usecases/sell_item.py
@@ -1,11 +1,21 @@
-from ...domain.models import Decision, Item, MarketInfo
+from ...domain.models import Decision, Item
 from ...domain.analyzers import BaseMarketAnalyzer
 
 
 class SellItemUseCase:
+    "Класс `SellItemUseCase` берет предмет и использует анализатор рынка для расчета цены продажи."
 
     def __init__(self, analyzer: BaseMarketAnalyzer) -> None:
         self._analyzer = analyzer
 
     def execute(self, item: Item) -> Decision:
+        """
+        Вычисляет решение о продаже предмета, используя анализатор рынка.
+
+        Args:
+            item (Item): Объект предмета, для которого нужно принять решение о продаже.
+
+        Returns:
+            Decision: Решение о продаже, содержащее информацию о цене и других параметрах.
+        """
         return self._analyzer.calc_selling_price(item)
--- services/seller/domain/analyzers/base.py
+++ services/seller/domain/analyzers/base.py
@@ -3,10 +3,27 @@ from ..models import MarketInfo, Item, Decision
 
 
 class BaseMarketAnalyzer(ABC):
+    """Базовый класс для аналитических моделей рынка.
+
+    Предоставляет интерфейс для расчета цен продажи предметов на рынке Dota.
+    """
 
     def __init__(self, market_info: MarketInfo) -> None:
+        """Инициализирует анализатор информацией о рынке.
+
+        Args:
+            market_info: Объект с информацией о рынке.
+        """
         self._info = market_info
 
     @abstractmethod
     def calc_selling_price(self, item: Item) -> Decision:
+        """Вычисляет решение о цене продажи предмета.
+
+        Args:
+            item: Объект предмета, для которого рассчитывается цена продажи.
+
+        Returns:
+            Решение о цене продажи.
+        """
         raise NotImplementedError()
