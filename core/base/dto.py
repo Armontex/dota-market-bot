f1a4548b185be97c4aaaf090d1a7ef5dc1bad036
Armontex <windle1337@gmail.com>
2026-02-07 22:23:46 +0500

    refactor(dto): Added bug report with deprecated decorator

--- core/base/dto.py
+++ core/base/dto.py
@@ -4,6 +4,8 @@ from common.decorators import emit_runtime_warning
 
 
 @emit_runtime_warning
-@deprecated("Use common.dto.DTO", stacklevel=2)
+@deprecated(
+    "Use common.dto.DTO", stacklevel=2
+)  # BUG Слетают все тесты, в которых используется этот декоратор
 class DTO(BaseModel):
     model_config = ConfigDict(frozen=True, arbitrary_types_allowed=True)
