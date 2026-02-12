c2da14af222b5a28f570322434a38f45ecd014e9
Armontex <windle1337@gmail.com>
2026-02-12 21:07:00 +0500

    refactor(tests): удалены не актуальные тесты dota_market

--- tests/infra/clients/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from typing import AsyncGenerator
-from infra.clients.common import AsyncHTTPSession
-
-
-@pytest.fixture
-async def async_http_session() -> AsyncGenerator[AsyncHTTPSession, None]:
-    async with AsyncHTTPSession() as session:
-        yield session
